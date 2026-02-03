dfcacddc7527c991c78fb04dc56323959e8291a5
Vlad Nyupa <v.nyuppa@gmail.com>
2026-02-03 13:45:23 +0300

    added API queries and types

--- src/api/tasks.js
+++ src/api/tasks.js
@@ -30,3 +30,50 @@ export async function runTask(id, payload) {
   const { data } = await axios.post(`${baseUrl}/${id}/run/`, payload);
   return data;
 }
+
+export async function fetchTemplates(params = {}) {
+  try {
+    const { data } = await axios.get(`${baseUrl}/templates/`, {
+      params: params,
+    });
+    return data;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export async function fetchTemplate(id, params = {}) {
+  try {
+    const { data } = await axios.get(`${baseUrl}/templates/${id}/`, {
+      params: params,
+    });
+    return data;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export async function createTemplate(payload) {
+  const { data } = await axios.post(`${baseUrl}/templates/`, payload);
+  return data;
+}
+
+export async function updateTemplate(id, payload) {
+  const { data } = await axios.put(`${baseUrl}/templates/${id}/`, payload);
+  return data;
+}
+
+export async function patchTemplate(id, payload) {
+  const { data } = await axios.patch(`${baseUrl}/templates/${id}/`, payload);
+  return data;
+}
+
+export async function deleteTemplate(id) {
+  const { data } = await axios.delete(`${baseUrl}/templates/${id}/`);
+  return data;
+}
+
+export async function runTemplate(id, payload) {
+  const { data } = await axios.post(`${baseUrl}/templates/${id}/run/`, payload);
+  return data;
+}
