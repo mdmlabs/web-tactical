43b884c39c20092b5cdc60fd166b56b63d12c2a4
itlaborato <it@laborato.org>
2026-07-28 12:35:14 +0300

    fix(light): stop polling unavailable alerts API

--- src/components/AlertsIcon.vue
+++ src/components/AlertsIcon.vue
@@ -1,5 +1,12 @@
 <template>
-  <q-btn flat dense class="alerts-button" no-caps no-wrap>
+  <q-btn
+    v-if="!isLightBuild"
+    flat
+    dense
+    class="alerts-button"
+    no-caps
+    no-wrap
+  >
     <svg viewBox="0 0 448 512" class="bell">
       <path
         d="M224 0c-17.7 0-32 14.3-32 32V49.9C119.5 61.4 64 124.2 64 200v33.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V200c0-75.8-55.5-138.6-128-150.1V32c0-17.7-14.3-32-32-32zm0 96h8c57.4 0 104 46.6 104 104v33.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V200c0-57.4 46.6-104 104-104h8zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z"
@@ -77,6 +84,7 @@ import { mapState } from "vuex";
 import mixins from "@/mixins/mixins";
 import AlertsOverview from "@/components/modals/alerts/AlertsOverview.vue";
 import { getTimeLapse } from "@/utils/format";
+import { BUILD_PRODUCT_EDITION } from "@/config/productEdition";
 
 export default {
   name: "AlertsIcon",
@@ -99,6 +107,9 @@ export default {
       "dash_warning_color",
       "dash_negative_color",
     ]),
+    isLightBuild() {
+      return BUILD_PRODUCT_EDITION === "light";
+    },
     badgeColor() {
       const severities = this.topAlerts.map((alert) => alert.severity);
 
@@ -109,6 +120,7 @@ export default {
   },
   methods: {
     getAlerts() {
+      if (this.isLightBuild) return;
       this.$axios.patch("alerts/", { top: 10 }).then((r) => {
         this.alertsCount = r.data.alerts_count;
         this.topAlerts = r.data.alerts;
@@ -194,6 +206,7 @@ export default {
     },
   },
   mounted() {
+    if (this.isLightBuild) return;
     this.getAlerts();
     this.pollAlerts();
   },
